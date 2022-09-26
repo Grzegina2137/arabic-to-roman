87b22eb3211bbd77c1ff2c1b68c0a3034d07a5a7
Grzegorz Szczepanek <grzegorzszczepanek48@gmail.com>
2022-09-26 18:27:48 +0200

    Create final.java

---
+++ final.java
@@ -0,0 +1,13 @@
+
+public class finalKeywords {
+
+	public static void main(String[] args) {
+		
+		final double PI = 3.14213;
+		//pi = 3; nie możemy zmienic finalów
+		
+		
+		//final to taki odpowiednik const z JS
+	}
+	
+}
