bb4475323b11b0f21b754cc577d403a4774088f0
Grzegorz Szczepanek <grzegorzszczepanek48@gmail.com>
2022-11-04 10:20:53 +0100

    Create snailArrayAlgorithm.js

---
+++ snailArrayAlgorithm.js
@@ -0,0 +1,56 @@
+
+let snail = function(arr) {
+//   if(arr.flat(2).length == 0){
+//     return []
+//   }
+
+
+   let lvl = 0;
+   let left = 0;
+   let width = arr.length - 1;
+   let bottom = 0;
+   let right = width;
+   let leftToRight = 0;
+   let rightToLeft = width-1;
+   let result = []
+  
+  while(result.length <= arr.length * arr.length){
+     
+    if(result.length == arr.length * arr.length) break
+    
+    while(leftToRight <= width){
+      result.push(arr[lvl][leftToRight]);
+      leftToRight++;
+    }
+    bottom++;
+    lvl++;
+    if(result.length == arr.length * arr.length) break
+    
+    while(lvl <= width){
+      result.push(arr[lvl][right])
+      lvl++;
+    }
+    right--;
+    width--;
+    lvl--;
+    if(result.length == arr.length * arr.length) break
+    
+    while(rightToLeft > left){
+      result.push(arr[lvl][rightToLeft]);
+      rightToLeft--;
+    }
+    rightToLeft = right - 1;
+    if(result.length == arr.length * arr.length) break
+    
+    while(lvl >= bottom ){
+      result.push(arr[lvl][left]);
+      lvl--;
+    }
+    lvl++;
+    left++;
+    leftToRight = left;
+  }
+  return result[0] == undefined ? [] : result
+}
+
+//console.log(snail([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20], [21, 22, 23, 24, 25]]));
